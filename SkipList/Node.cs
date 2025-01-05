deaecd91969e2e2332fc49fe7ca6863856fd2953
VeerShahCodes <shahveer1213@gmail.com>
2025-01-05 15:01:33 -0800

    made recursive add but I still need to fully understand how it works and if it works

---
+++ SkipList/Node.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SkipList
+{
+    public class Node<T>
+        where T : IComparable<T>
+    {
+        public T Value; // Value of the node
+        public Node<T> Next; // Rightward connection
+        public Node<T> Down; // Downward connection
+        public int Height => GetHeight(); // Vertical height of the node
+
+        public Node(T value) 
+        {
+            Value = value;
+            Next = null;
+            Down = null;
+        } // Fill out constructors
+        public Node(T value, Node<T> down) 
+        {
+            Value = value;
+            Down = down;
+            Next = null;
+        }
+        public Node(T value, Node<T> down,Node<T> next)
+        {
+            Value = value;
+            Down = down;
+            Next = next;
+        }
+        int GetHeight()
+        {
+            if (Down == null) return 1;
+            return Down.GetHeight() + 1;
+        }
+    }
+}
---
+++ SkipList/SkipList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkipList
+{
+    
+    public class SkipList<T> where T:IComparable<T>
+    {
+        Random random = new Random();
+        Node<T> Head;
+
+        public SkipList(T val)
+        {
+            Head = new Node<T>(val);
+        }
+        public int ChooseRandomHeight(Random rand)
+        {
+            int height = 1;
+            while(true)
+            {
+                int randNum = rand.Next(2);
+                if (randNum == 0) break;
+                else
+                {
+                    height++;
+                }
+            }
+            return height;
+
+        }
+
+        public void Add(T val)
+        {
+            int height = ChooseRandomHeight(random);
+            Head = RecAdd(Head, val, height);
+        }
+
+        Node<T> RecAdd(Node<T> Current,T val,int height)
+        {
+            //Base case
+            if (Current == null) return null;
+
+            //Down            
+            if(Current.Next == null || Current.Next.Value.CompareTo(Current.Value) > 0)
+            {
+                //Rec Case
+                var temp = RecAdd(Current.Down, val, height);
+                //Up
+                Node<T> addNode = new Node<T>(val, temp, Current.Next);
+                if (addNode.Height<=height)
+                {                   
+                    Current.Next = addNode;
+                    return addNode;
+                }
+                else
+                {
+                    return temp;
+                }
+
+               
+            }
+            else
+            {
+                //Rec Case
+                var temp = RecAdd(Current.Next, val, height);
+                //Up
+                return temp;
+
+            }
+
+            
+        }
+    }
+}
